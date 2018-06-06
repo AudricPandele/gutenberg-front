131d7ef6e601790aa433409a0597389d459d8345
Pierre <pi.brethes@gmail.com>
2018-06-06 23:25:00 +0200

    feat: first commit, react-router-dom, home comp

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './../App.css';
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="">
+        <p>Hello World !</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
